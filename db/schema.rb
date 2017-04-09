03e4d9d45d4236d5437ffe073550134b53ec1f5d
Kyle Tress <kyle@kyletress.com>
2017-04-09 16:12:42 -0700

    add isbn_13 to books

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170409215239) do
+ActiveRecord::Schema.define(version: 20170409231215) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20170409215239) do
     t.string   "subtitle"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "isbn_13"
   end
 
 end
